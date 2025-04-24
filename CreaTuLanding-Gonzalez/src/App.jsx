4bb55a11db255842a9538ae57bf20ce72f98296d
maximiliano836 <maxi10.02.00.g@gmail.com>
2025-04-24 20:10:46 -0300

    corrigiendo links

--- CreaTuLanding-Gonzalez/src/App.jsx
+++ CreaTuLanding-Gonzalez/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
+      <>
       <NavBar/>
       <ItemListContainer message="¡Bienvenido a TecnoStore!  Descubrí nuestros productos" />
-    </>
+      </>
+    </Router>
   );
 }
 
--- CreaTuLanding-Gonzalez/src/components/NavBar.jsx
+++ CreaTuLanding-Gonzalez/src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CartWidget from './CartWidget';
 import '../css/NavBar.css';
+import { Link } from 'react-router-dom';
+
 
 const NavBar = () => {
   return (
     <nav className='NavBar'>
-      <a href="/"><h1>TecnoStore</h1></a>
+      <Link to="/"><h1>TecnoStore</h1></Link>
       <a href="#">Inicio</a>
       <a href="#">Productos</a>
       <a href="#">Contacto</a>
